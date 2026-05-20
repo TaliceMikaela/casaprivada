5282c30f74037748dabec973cafea1b2a7f2bd14
TaliceMikaela <casadosprazeres@icloud.com>
2026-05-19 22:54:47 -0300

    Atualizando modelos

--- static/js/models-gallery-data.js
+++ static/js/models-gallery-data.js
@@ -1,61 +1,137 @@
-window.MODELS_GALLERY_DATA = [
+﻿window.MODELS_GALLERY_DATA =
+[
   {
-    name: 'Beatriz',
-    age: '19 anos',
-    city: 'Casa dos Prazeres',
-    tagline: 'Galeria individual',
-    description: 'Perfil com fotos selecionadas para divulgação no site.',
-    cover: 'static/img/modelos/Beatriz19anos/foto1.jpg',
-    gallery: [
-      { src: 'static/img/modelos/Beatriz19anos/foto1.jpg', title: 'Beatriz • Foto 1', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Beatriz19anos/foto2.jpg', title: 'Beatriz • Foto 2', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Beatriz19anos/foto3.jpg', title: 'Beatriz • Foto 3', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Beatriz19anos/foto4.jpg', title: 'Beatriz • Foto 4', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Beatriz19anos/foto5.jpg', title: 'Beatriz • Foto 5', description: 'Foto da galeria individual.' }
+    "name": "Beatriz",
+    "age": "19 anos",
+    "city": "Casa dos Prazeres",
+    "tagline": "Galeria individual",
+    "description": "Perfil com fotos selecionadas para divulgação no site.",
+    "cover": "static/img/modelos/Beatriz19anos/foto1.jpg",
+    "gallery": [
+      {
+        "src": "static/img/modelos/Beatriz19anos/foto1.jpg",
+        "title": "Beatriz • Foto 1",
+        "description": "Foto da galeria individual."
+      },
+      {
+        "src": "static/img/modelos/Beatriz19anos/foto2.jpg",
+        "title": "Beatriz • Foto 2",
+        "description": "Foto da galeria individual."
+      },
+      {
+        "src": "static/img/modelos/Beatriz19anos/foto3.jpg",
+        "title": "Beatriz • Foto 3",
+        "description": "Foto da galeria individual."
+      },
+      {
+        "src": "static/img/modelos/Beatriz19anos/foto4.jpg",
+        "title": "Beatriz • Foto 4",
+        "description": "Foto da galeria individual."
+      },
+      {
+        "src": "static/img/modelos/Beatriz19anos/foto5.jpg",
+        "title": "Beatriz • Foto 5",
+        "description": "Foto da galeria individual."
+      }
     ]
   },
   {
-    name: 'Natasha',
-    age: '26 anos',
-    city: 'Casa dos Prazeres',
-    tagline: 'Galeria individual',
-    description: 'Perfil com fotos selecionadas para divulgação no site.',
-    cover: 'static/img/modelos/Natasha26anos/foto1.jpg',
-    gallery: [
-      { src: 'static/img/modelos/Natasha26anos/foto1.jpg', title: 'Natasha • Foto 1', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Natasha26anos/foto2.jpg', title: 'Natasha • Foto 2', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Natasha26anos/foto3.jpg', title: 'Natasha • Foto 3', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Natasha26anos/foto4.jpg', title: 'Natasha • Foto 4', description: 'Foto da galeria individual.' }
+    "name": "Natasha",
+    "age": "26 anos",
+    "city": "Casa dos Prazeres",
+    "tagline": "Galeria individual",
+    "description": "Perfil com fotos selecionadas para divulgação no site.",
+    "cover": "static/img/modelos/Natasha26anos/foto1.jpg",
+    "gallery": [
+      {
+        "src": "static/img/modelos/Natasha26anos/foto1.jpg",
+        "title": "Natasha • Foto 1",
+        "description": "Foto da galeria individual."
+      },
+      {
+        "src": "static/img/modelos/Natasha26anos/foto2.jpg",
+        "title": "Natasha • Foto 2",
+        "description": "Foto da galeria individual."
+      },
+      {
+        "src": "static/img/modelos/Natasha26anos/foto3.jpg",
+        "title": "Natasha • Foto 3",
+        "description": "Foto da galeria individual."
+      },
+      {
+        "src": "static/img/modelos/Natasha26anos/foto4.jpg",
+        "title": "Natasha • Foto 4",
+        "description": "Foto da galeria individual."
+      }
     ]
   },
   {
-    name: 'Nina',
-    age: '19 anos',
-    city: 'Casa dos Prazeres',
-    tagline: 'Galeria individual',
-    description: 'Perfil com fotos selecionadas para divulgação no site.',
-    cover: 'static/img/modelos/Nina19anos/foto1.jpg',
-    gallery: [
-      { src: 'static/img/modelos/Nina19anos/foto1.jpg', title: 'Nina • Foto 1', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Nina19anos/foto2.jpg', title: 'Nina • Foto 2', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Nina19anos/foto3.jpg', title: 'Nina • Foto 3', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Nina19anos/foto4.jpg', title: 'Nina • Foto 4', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Nina19anos/foto5.jpg', title: 'Nina • Foto 5', description: 'Foto da galeria individual.' }
+    "name": "Nina",
+    "age": "19 anos",
+    "city": "Casa dos Prazeres",
+    "tagline": "Galeria individual",
+    "description": "Perfil com fotos selecionadas para divulgação no site.",
+    "cover": "static/img/modelos/Nina19anos/foto1.jpg",
+    "gallery": [
+      {
+        "src": "static/img/modelos/Nina19anos/foto1.jpg",
+        "title": "Nina • Foto 1",
+        "description": "Foto da galeria individual."
+      },
+      {
+        "src": "static/img/modelos/Nina19anos/foto2.jpg",
+        "title": "Nina • Foto 2",
+        "description": "Foto da galeria individual."
+      },
+      {
+        "src": "static/img/modelos/Nina19anos/foto3.jpg",
+        "title": "Nina • Foto 3",
+        "description": "Foto da galeria individual."
+      },
+      {
+        "src": "static/img/modelos/Nina19anos/foto4.jpg",
+        "title": "Nina • Foto 4",
+        "description": "Foto da galeria individual."
+      },
+      {
+        "src": "static/img/modelos/Nina19anos/foto5.jpg",
+        "title": "Nina • Foto 5",
+        "description": "Foto da galeria individual."
+      }
     ]
   },
   {
-    name: 'Natália',
-    age: '18 anos',
-    city: 'Casa dos Prazeres',
-    tagline: 'Galeria individual',
-    description: 'Perfil com fotos selecionadas para divulgação no site.',
-    cover: 'static/img/modelos/Natalia18anos/foto1.jpg',
-    gallery: [
-      { src: 'static/img/modelos/Natalia18anos/foto1.jpg', title: 'Natália • Foto 1', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Natalia18anos/foto2.jpg', title: 'Natália • Foto 2', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Natalia18anos/foto3.jpg', title: 'Natália • Foto 3', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Natalia18anos/foto4.jpg', title: 'Natália • Foto 4', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Natalia18anos/foto5.jpg', title: 'Natália • Foto 5', description: 'Foto da galeria individual.' }
+    "name": "Natália",
+    "age": "18 anos",
+    "city": "Casa dos Prazeres",
+    "tagline": "Galeria individual",
+    "description": "Perfil com fotos selecionadas para divulgação no site.",
+    "cover": "static/img/modelos/Natalia18anos/foto1.jpg",
+    "gallery": [
+      {
+        "src": "static/img/modelos/Natalia18anos/foto1.jpg",
+        "title": "Natália • Foto 1",
+        "description": "Foto da galeria individual."
+      },
+      {
+        "src": "static/img/modelos/Natalia18anos/foto2.jpg",
+        "title": "Natália • Foto 2",
+        "description": "Foto da galeria individual."
+      },
+      {
+        "src": "static/img/modelos/Natalia18anos/foto3.jpg",
+        "title": "Natália • Foto 3",
+        "description": "Foto da galeria individual."
+      },
+      {
+        "src": "static/img/modelos/Natalia18anos/foto4.jpg",
+        "title": "Natália • Foto 4",
+        "description": "Foto da galeria individual."
+      },
+      {
+        "src": "static/img/modelos/Natalia18anos/foto5.jpg",
+        "title": "Natália • Foto 5",
+        "description": "Foto da galeria individual."
+      }
     ]
-  }
-];
+  }];
