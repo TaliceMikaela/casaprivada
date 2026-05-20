813eff9c07587f41f8876c9d40b4eba9b71ce411
TaliceMikaela <casadosprazeres@icloud.com>
2026-05-19 22:17:18 -0300

    Atualização Galeria de Modelos

--- static/js/models-gallery-data.js
+++ static/js/models-gallery-data.js
@@ -1,20 +1,5 @@
 window.MODELS_GALLERY_DATA = [
   {
-    name: 'Alice',
-    age: '19 anos',
-    city: 'Casa dos Prazeres',
-    tagline: 'Galeria individual',
-    description: 'Perfil com fotos selecionadas para divulgação no site.',
-    cover: 'static/img/modelos/Alice19anos/foto1.jpg',
-    gallery: [
-      { src: 'static/img/modelos/Alice19anos/foto1.jpg', title: 'Alice • Foto 1', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Alice19anos/foto2.jpg', title: 'Alice • Foto 2', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Alice19anos/foto3.jpg', title: 'Alice • Foto 3', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Alice19anos/foto4.jpg', title: 'Alice • Foto 4', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Alice19anos/foto5.jpg', title: 'Alice • Foto 5', description: 'Foto da galeria individual.' }
-    ]
-  },
-  {
     name: 'Beatriz',
     age: '19 anos',
     city: 'Casa dos Prazeres',
@@ -27,19 +12,6 @@ window.MODELS_GALLERY_DATA = [
       { src: 'static/img/modelos/Beatriz19anos/foto3.jpg', title: 'Beatriz • Foto 3', description: 'Foto da galeria individual.' },
       { src: 'static/img/modelos/Beatriz19anos/foto4.jpg', title: 'Beatriz • Foto 4', description: 'Foto da galeria individual.' },
       { src: 'static/img/modelos/Beatriz19anos/foto5.jpg', title: 'Beatriz • Foto 5', description: 'Foto da galeria individual.' }
-    ]
-  },
-  {
-    name: 'Amanda',
-    age: '26 anos',
-    city: 'Casa dos Prazeres',
-    tagline: 'Galeria individual',
-    description: 'Perfil com fotos selecionadas para divulgação no site.',
-    cover: 'static/img/modelos/Amanda26anos/foto1.jpg',
-    gallery: [
-      { src: 'static/img/modelos/Amanda26anos/foto1.jpg', title: 'Amanda • Foto 1', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Amanda26anos/foto2.jpg', title: 'Amanda • Foto 2', description: 'Foto da galeria individual.' },
-      { src: 'static/img/modelos/Amanda26anos/foto3.jpg', title: 'Amanda • Foto 3', description: 'Foto da galeria individual.' }
     ]
   },
   {
