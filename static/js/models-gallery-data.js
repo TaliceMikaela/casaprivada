ff86f1e16cfc781606ea80e014be755423feb3b9
TaliceMikaela <casadosprazeres@icloud.com>
2026-06-03 18:55:33 -0300

    Atualizando modelos

--- static/js/models-gallery-data.js
+++ static/js/models-gallery-data.js
@@ -1,44 +1,6 @@
 ﻿window.MODELS_GALLERY_DATA =
 [
   {
-    "id": "natalia18anos",
-    "name": "Natalia",
-    "age": "18 anos",
-    "sign": "",
-    "city": "",
-    "tagline": "Clique para ver a galeria individual.",
-    "description": "Galeria individual de Natalia.",
-    "folder": "static/img/modelos/Natalia18anos_20260520_184010",
-    "cover": "static/img/modelos/Natalia18anos_20260520_184010/foto1.jpg",
-    "gallery": [
-      {
-        "src": "static/img/modelos/Natalia18anos_20260520_184010/foto1.jpg",
-        "title": "Natalia • Foto 1",
-        "description": "Clique para ampliar"
-      },
-      {
-        "src": "static/img/modelos/Natalia18anos_20260520_184010/foto2.jpg",
-        "title": "Natalia • Foto 2",
-        "description": "Clique para ampliar"
-      },
-      {
-        "src": "static/img/modelos/Natalia18anos_20260520_184010/foto3.jpg",
-        "title": "Natalia • Foto 3",
-        "description": "Clique para ampliar"
-      },
-      {
-        "src": "static/img/modelos/Natalia18anos_20260520_184010/foto4.jpg",
-        "title": "Natalia • Foto 4",
-        "description": "Clique para ampliar"
-      },
-      {
-        "src": "static/img/modelos/Natalia18anos_20260520_184010/foto5.jpg",
-        "title": "Natalia • Foto 5",
-        "description": "Clique para ampliar"
-      }
-    ]
-  },
-  {
     "id": "alice19anos-20260523-172918",
     "name": "Alice",
     "age": "19 anos",
@@ -80,4 +42,48 @@
         "description": "Clique para ampliar"
       }
     ]
-  }];
+  },
+  {
+    "id": "natalia18anos-20260603-185126",
+    "name": "Natalia",
+    "age": "18 anos",
+    "sign": "",
+    "city": "",
+    "tagline": "Clique para ver a galeria individual.",
+    "description": "Galeria individual de Natalia.",
+    "folder": "static/img/modelos/Natalia18anos_20260603_185126",
+    "cover": "static/img/modelos/Natalia18anos_20260603_185126/foto1.jpg",
+    "gallery": [
+      {
+        "src": "static/img/modelos/Natalia18anos_20260603_185126/foto1.jpg",
+        "alt": "Natalia • Foto 1",
+        "title": "Natalia • Foto 1",
+        "description": "Clique para ampliar"
+      },
+      {
+        "src": "static/img/modelos/Natalia18anos_20260603_185126/foto2.jpg",
+        "alt": "Natalia • Foto 2",
+        "title": "Natalia • Foto 2",
+        "description": "Clique para ampliar"
+      },
+      {
+        "src": "static/img/modelos/Natalia18anos_20260603_185126/foto3.jpg",
+        "alt": "Natalia • Foto 3",
+        "title": "Natalia • Foto 3",
+        "description": "Clique para ampliar"
+      },
+      {
+        "src": "static/img/modelos/Natalia18anos_20260603_185126/foto4.jpg",
+        "alt": "Natalia • Foto 4",
+        "title": "Natalia • Foto 4",
+        "description": "Clique para ampliar"
+      },
+      {
+        "src": "static/img/modelos/Natalia18anos_20260603_185126/foto5.jpg",
+        "alt": "Natalia • Foto 5",
+        "title": "Natalia • Foto 5",
+        "description": "Clique para ampliar"
+      }
+    ]
+  }
+];
