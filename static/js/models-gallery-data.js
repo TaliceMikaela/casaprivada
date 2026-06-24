ecc1b3cf6f7199bab2c7af0a214d1bf2a8274f57
TaliceMikaela <casadosprazeres@icloud.com>
2026-06-23 23:18:58 -0300

    atualizando app

--- static/js/models-gallery-data.js
+++ static/js/models-gallery-data.js
@@ -87,49 +87,6 @@
     ]
   },
   {
-    "id": "mirela28anos-20260609-191138",
-    "name": "Mirela",
-    "age": "28 anos",
-    "sign": "",
-    "city": "",
-    "tagline": "Clique para ver a galeria individual.",
-    "description": "Galeria individual de Mirela.",
-    "folder": "static/img/modelos/Mirela28anos_20260609_191138",
-    "cover": "static/img/modelos/Mirela28anos_20260609_191138/foto1.jpg",
-    "gallery": [
-      {
-        "src": "static/img/modelos/Mirela28anos_20260609_191138/foto1.jpg",
-        "alt": "Mirela • Foto 1",
-        "title": "Mirela • Foto 1",
-        "description": "Clique para ampliar"
-      },
-      {
-        "src": "static/img/modelos/Mirela28anos_20260609_191138/foto2.jpg",
-        "alt": "Mirela • Foto 2",
-        "title": "Mirela • Foto 2",
-        "description": "Clique para ampliar"
-      },
-      {
-        "src": "static/img/modelos/Mirela28anos_20260609_191138/foto3.jpg",
-        "alt": "Mirela • Foto 3",
-        "title": "Mirela • Foto 3",
-        "description": "Clique para ampliar"
-      },
-      {
-        "src": "static/img/modelos/Mirela28anos_20260609_191138/foto4.jpg",
-        "alt": "Mirela • Foto 4",
-        "title": "Mirela • Foto 4",
-        "description": "Clique para ampliar"
-      },
-      {
-        "src": "static/img/modelos/Mirela28anos_20260609_191138/foto5.jpg",
-        "alt": "Mirela • Foto 5",
-        "title": "Mirela • Foto 5",
-        "description": "Clique para ampliar"
-      }
-    ]
-  },
-  {
     "id": "ashleycubana23anos-20260610-212234",
     "name": "Ashley Cubana",
     "age": "23 anos",
@@ -168,55 +125,6 @@
         "src": "static/img/modelos/AshleyCubana23anos_20260610_212234/foto5.jpg",
         "alt": "AshleyCubana • Foto 5",
         "title": "AshleyCubana • Foto 5",
-        "description": "Clique para ampliar"
-      }
-    ]
-  },
-  {
-    "id": "isisferrari22anos-20260610-220227",
-    "name": "Isis Ferrari",
-    "age": "22 anos",
-    "sign": "",
-    "city": "",
-    "tagline": "Clique para ver a galeria individual.",
-    "description": "Galeria individual de Isis Ferrari.",
-    "folder": "static/img/modelos/IsisFerrari22anos_20260610_220227",
-    "cover": "static/img/modelos/IsisFerrari22anos_20260610_220227/foto1.jpg",
-    "gallery": [
-      {
-        "src": "static/img/modelos/IsisFerrari22anos_20260610_220227/foto1.jpg",
-        "alt": "Isis Ferrari • Foto 1",
-        "title": "Isis Ferrari • Foto 1",
-        "description": "Clique para ampliar"
-      },
-      {
-        "src": "static/img/modelos/IsisFerrari22anos_20260610_220227/foto2.jpg",
-        "alt": "Isis Ferrari • Foto 2",
-        "title": "Isis Ferrari • Foto 2",
-        "description": "Clique para ampliar"
-      },
-      {
-        "src": "static/img/modelos/IsisFerrari22anos_20260610_220227/foto3.jpg",
-        "alt": "Isis Ferrari • Foto 3",
-        "title": "Isis Ferrari • Foto 3",
-        "description": "Clique para ampliar"
-      },
-      {
-        "src": "static/img/modelos/IsisFerrari22anos_20260610_220227/foto4.jpg",
-        "alt": "Isis Ferrari • Foto 4",
-        "title": "Isis Ferrari • Foto 4",
-        "description": "Clique para ampliar"
-      },
-      {
-        "src": "static/img/modelos/IsisFerrari22anos_20260610_220227/foto5.jpg",
-        "alt": "Isis Ferrari • Foto 5",
-        "title": "Isis Ferrari • Foto 5",
-        "description": "Clique para ampliar"
-      },
-      {
-        "src": "static/img/modelos/IsisFerrari22anos_20260610_220227/foto6.jpg",
-        "alt": "Isis Ferrari • Foto 6",
-        "title": "Isis Ferrari • Foto 6",
         "description": "Clique para ampliar"
       }
     ]
